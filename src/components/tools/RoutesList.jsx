496fea239025516990b68b33c85e954132a9905a
Sergio León <sergioleon4004@hotmail.com>
2023-03-01 09:37:52 -0500

    style: routes have been organized

--- src/components/tools/RoutesList.jsx
+++ src/components/tools/RoutesList.jsx
@@ -3,57 +3,57 @@ const host = import.meta.env.VITE_SERVER_URL_AUD;
 export default {
   host: host,
   api: {
-    auth:{
-      login:`${host}/auth/login`,
+    auth: {
+      login: `${host}/api/auth/login`,
     },
-    roles:{
-      read:`${host}/read-roles`
+    roles: {
+      read: `${host}/api/read-roles`,
     },
     companies: {
-      create: `${host}/companies/create`,
-      read:{
-        read_companies_selector:`${host}/companies/read-companies-selector`,
+      create: `${host}/api/companies/create`,
+      read: {
+        read_companies_selector: `${host}/api/companies/read-companies-selector`,
       },
-      requirements:{
-        create: `${host}/companies/requirements/create`,
-        update: `${host}/companies/requirements/update`,
+      requirements: {
+        create: `${host}/api/companies/requirements/create`,
+        update: `${host}/api/companies/requirements/update`,
         read: {
-          read_requirementsByclients:`${host}/companies/requirements/requirementsByclients`,
-          read_pending:`${host}/companies/requirements/pending`,
-          read_accept:`${host}/companies/requirements/accepted`,
-          read_finished:`${host}/companies/requirements/finished`,
-          read_requirementsByadmin:`${host}/companies/requirements/requirementsByadmin`,
-          read_requirementSelector:`${host}/companies/requirements/requirements-selector`,
-          read_StateSelector:`${host}/companies/requirements/stateselector`,
-        }
-      }
-    },
-    assignment:{
-      create: `${host}/assignment/create`,
-      update: `${host}/assignment/update`,
-      read:{
-        select:`${host}/assignment/read/select`,
-        read_assigments:`${host}/assignment/read/read_assigments`,
+          read_requirementsByclients: `${host}/api/companies/requirements/requirementsByclients`,
+          read_pending: `${host}/api/companies/requirements/pending`,
+          read_accept: `${host}/api/companies/requirements/accepted`,
+          read_finished: `${host}/api/companies/requirements/finished`,
+          read_requirementsByadmin: `${host}/api/companies/requirements/requirementsByadmin`,
+          read_requirementSelector: `${host}/api/companies/requirements/requirements-selector`,
+          read_StateSelector: `${host}/api/companies/requirements/stateselector`,
+        },
       },
-      developer:{
-        create:`${host}/assignment/developers/create`,
-        delete:`${host}/assignment/developers/delete`,
-        update:`${host}/assignment/developers/update`,
-        read:{
-          bydevelopers:`${host}/assignment/developers/read/bydevelopers`,
-          assigment:`${host}/assignment/developers/read/assigment`,
-          tasks_assigned_status:`${host}/assignment/developers/read/tasks-assigned-status`,
-          tasks_finished_status:`${host}/assignment/developers/read/tasks-finished-status`,
-        }
-      }
     },
-    developer:{
-        create:`${host}/developers/create`,
-        update:`${host}/developers/update`,
-      read:{
-        full:`${host}/developers/read/`,
-        select:`${host}/developers/read/select`,
-      }
-    }
+    assignment: {
+      create: `${host}/api/assignment/create`,
+      update: `${host}/api/assignment/update`,
+      read: {
+        select: `${host}/api/assignment/read/select`,
+        read_assigments: `${host}/api/assignment/read/read_assigments`,
+      },
+      developer: {
+        create: `${host}/api/assignment/developers/create`,
+        delete: `${host}/api/assignment/developers/delete`,
+        update: `${host}/api/assignment/developers/update`,
+        read: {
+          bydevelopers: `${host}/api/assignment/developers/read/bydevelopers`,
+          assigment: `${host}/api/assignment/developers/read/assigment`,
+          tasks_assigned_status: `${host}/api/assignment/developers/read/tasks-assigned-status`,
+          tasks_finished_status: `${host}/api/assignment/developers/read/tasks-finished-status`,
+        },
+      },
+    },
+    developer: {
+      create: `${host}/api/developers/create`,
+      update: `${host}/api/developers/update`,
+      read: {
+        full: `${host}/api/developers/read/`,
+        select: `${host}/api/developers/read/select`,
+      },
+    },
   },
 };
