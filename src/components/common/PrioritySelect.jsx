e29af100a11047d4241dbccdad15063220168cf6
Sergio León <sergioleon4004@hotmail.com>
2023-02-28 23:43:01 -0500

    ci: option has been added to select

--- src/components/common/PrioritySelect.jsx
+++ src/components/common/PrioritySelect.jsx
@@ -1,27 +1,23 @@
-import React, { useState } from "react";
 const PrioritySelect = ({ value, setValue, required }) => {
-
-  const [prioritys, setPrioritys] = useState(["Nuevo","Novedad"]);
   return (
     <div className="form__demo-container-name" style={{ marginBottom: "20px" }}>
       <label className={"form__label-name"}>Seleccione Tipo</label>
       <select
-       style={{width:"98%"}}
+        style={{ width: "98%" }}
         className={"form__select-name"}
         value={value}
         onChange={(e) => setValue(e.target.value)}
         required={required}
       >
-      {prioritys.map( (item, index)=>(
-        <option 
-          key={index}
-          value={item}
-        >{item}
+        <option value={""}>
+          {"Seleccione"}
         </option>
-      ))
 
-
-      }
+        {["Nuevo", "Novedad"].map((item, index) => (
+          <option key={index} value={item}>
+            {item}
+          </option>
+        ))}
       </select>
     </div>
   );
